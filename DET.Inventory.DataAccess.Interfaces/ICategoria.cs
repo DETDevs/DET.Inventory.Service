c4b1129759cad9837da7b3117939361228385702
douglas-i <hdouglasu@gmail.com>
2025-01-27 22:20:25 -0600

    t-dev: add categoryController

--- DET.Inventory.DataAccess.Interfaces/ICategoria.cs
+++ DET.Inventory.DataAccess.Interfaces/ICategoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DET.Inventory.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace DET.Inventory.DataAccess.Interfaces
 {
     public interface ICategoria
     {
+        Task<List<Categoria>> ListarCategorias();
     }
 }
